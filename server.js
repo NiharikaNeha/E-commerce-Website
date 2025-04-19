98e47606cf5cba751badb9c15a3622969112f4b1
Niharika Swain <swainniharika27@gmail.com>
2025-04-19 21:20:20 +0530

    file uploaded

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("Now All the files are uploaded on github page")
+// Accessing The Needs
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+//Connect To The MongoDb Database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO THE API TESTING...");
+});
+
+// Adding All API Routes
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server Is Running On http://localhost:${PORT}`);
+});
