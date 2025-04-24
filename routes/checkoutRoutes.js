3f39176547ea263b47fca82506d80785ae923aff
Niharika Swain <swainniharika27@gmail.com>
2025-04-24 21:37:58 +0530

    File Uploaded

---
+++ routes/checkoutRoutes.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const Checkout = require("../models/Checkout");
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
+const Order = require("../models/Order");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+//@route POST /api/checkout
+//@desc Creating A New Checkout Session
+//@Access Private
+router.post("/", protect, async (req, res) => {
+  const { checkoutItems, shippingAddress, paymentMethod, totalPrice } =
+    req.body;
+
+  if (!checkoutItems || checkoutItems.length === 0) {
+    return res.status(400).json({ message: "No Item In Checkout" });
+  }
+
+  try {
+    //Create A New Checkout Session
+    const newCheckout = await Checkout.create({
+      user: req.user._id,
+      checkoutItems: checkoutItems,
+      shippingAddress,
+      paymentMethod,
+      totalPrice,
+      paymentStatus: "Pending",
+      isPaid: false,
+    });
+    console.log(`Checkout Created For User: ${req.user._id}`);
+    res.status(201).json(newCheckout);
+  } catch (error) {
+    console.error("Error Creating Checkout Session:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@route PUT /api/checkout/:id/pay
+//@desc Update Checkout To Make As Paid After Suuccessful Payment
+//@Access Private
+router.put("/:id/pay", protect, async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout Not Found" });
+    }
+
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+      await checkout.save();
+
+      res.status(200).json(checkout);
+    } else {
+      res.status(400).json({ message: "Invalid Payment Status" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@route POST /api/checkout/:id/finalize
+//@desc Finalize Checkout And Convert To An Order After Payment Confirmation
+//@Access Private
+router.post("/:id/finalize", protect, async (req, res) => {
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout Not Found" });
+    }
+    if (checkout.isPaid && !checkout.isFinalized) {
+      //Create Final Order Based On The Checkout Details
+      const finalOrder = await Order.create({
+        user: checkout.user,
+        orderItems: checkout.checkoutItems,
+        shippingAddress: checkout.shippingAddress,
+        paymentMethod: checkout.paymentMethod,
+        totalPrice: checkout.totalPrice,
+        isPaid: true,
+        paidAt: checkout.paidAt,
+        isDelivered: false,
+        paymentStatus: "paid",
+        paymentDetails: checkout.paymentDetails,
+      });
+
+      //Mark The Checkout As Finalized
+      checkout.isFinalized = true;
+      checkout.finalizedAt = Date.now();
+      await checkout.save();
+      //Delete The Cart Associated With The User
+      await Cart.findOneAndDelete({ user: checkout.user });
+      res.status(201).json(finalOrder);
+    } else if (checkout.isFinalized) {
+      res.status(400).json({ message: "Checkout Already Finalized" });
+    } else {
+      res.status(400).json({ message: "Checkout Is Not Paid" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
